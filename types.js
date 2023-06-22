578b1810011710ec9264c52f8330de75d8add171
SimoMassaro52 <simonesimomassaro@gmail.com>
2023-06-22 16:40:00 +0200

    starting commit

---
+++ types.js
@@ -0,0 +1,15 @@
+//This is just the list of ALL the types present in regular JavaScript
+
+//PRIMITIVES:
+
+//Boolean
+const boolean1 = true;
+const boolean2 = false;
+
+//Number
+const fiftyFive = 55;
+
+//String
+const name = "Tom";
+
+//BigInt
